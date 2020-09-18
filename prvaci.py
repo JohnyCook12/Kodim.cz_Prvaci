9ac2d34b1048bb68b632b1ffb1ce51781a595ec4
JohnyCook12 <john.yy@seznam.cz>
2020-09-18 18:42:16 +0200

    Created prvaci.py + docstring

---
+++ prvaci.py
@@ -0,0 +1,9 @@
+
+"""Program PRVACI:
+    1) nacte data z studenti.txt jako tabulku seznamu - kazdy radek = vnoreny seznam.
+    2) pridame sloupec udavajici vek
+    3) pridame sloupec s pohlavim
+    4) pridame sloupec email (=5pismen prijmeni + 3pismena jmena @hybrid.eu
+
+"""
+
